71e4ad929cdba507ba09972990641a691767e3cc
progwithibi <progwithibi@gmail.com>
2024-06-11 13:22:27 +0500

    Variables ,Operator & Operator Precedence

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,6 @@
-alert("Hello")
+const defaultValue = 0;
+let currentResult = defaultValue;
+
+currentResult = currentResult + 10 * (30 / 44) *200;
+
+outputResult(currentResult,`${defaultValue} + 10 * (30 / 44) *200 `)
